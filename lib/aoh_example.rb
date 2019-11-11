68ccdff11bacc9ae9891f3b0ec3dc0bffd335bbb
Sean Tarzy <seantarzy@gmail.com>
2019-11-11 21:46:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,13 +19,14 @@ array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
 def literal_aoh
-  
+  return assembled_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
+  aoh[row][key] = new_value
+  return aoh[row][key]
 end
